53592ed4267f858a7a6836f893296be6add422d8
Tuda <nguyentuandatk16a@gmail.com>
2023-12-07 01:23:24 +0700

    Update Preprocessing.py

--- Data_and_AI/Preprocessing.py
+++ Data_and_AI/Preprocessing.py
@@ -1,57 +1,24 @@
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
-import pandas as pd
 from scipy.interpolate import interp1d
-from sklearn.decomposition import PCA
-
-y = np.loadtxt("Data/Subject_1_nhammat.txt")
-mean = np.mean(y)
-std = np.std(y)
 
 
-# Design the band-pass filter
-
-
-# Lọc mảng theo phân phối chuẩn
 def filter_data(data):
-    filtered_data = data[(np.abs(data) <= 256)]
+    # Bandpass filter
+    band = [0.5 / (0.5 * 512), 40 / (0.5 * 512)]
+    b, a = sp.signal.butter(4, band, btype='band', analog=False, output='ba')
+    data = sp.signal.lfilter(b, a, data)
+
+    # filter for EMG by interpolated
+    filtered_data = data[(np.abs(data) <= 128)]
     x = np.arange(len(filtered_data))
     interpolated_data = interp1d(x, filtered_data)(np.linspace(0, len(filtered_data) - 1, len(data)))
     return interpolated_data
 
-# 0.5-50 Hz
-# trim = np.where((f >= 0.5) & (f <= 50))[0]
-# f = f[trim]
-# Zxx = Zxx[trim, :]
 
-plt.figure(0)
-plt.plot(y)
-#
-# plt.figure(1)
-# plt.pcolormesh(t, f, np.abs(Zxx), vmin=-1, vmax=10, shading='auto')
-# plt.title('STFT Magnitude')
-# plt.ylim(0.5, 50)
-# plt.ylabel('Frequency [Hz]')
-# plt.xlabel('Time [sec]')
-# print(Zxx.shape)
-
-# plt.show()
-
-def FeatureExtract(y):
-    band = [0.5 / (0.5 * 512), 40 / (0.5 * 512)]
-    b, a = sp.signal.butter(4, band, btype='band', analog=False, output='ba')
-    y = sp.signal.lfilter(b, a, y)
-
-    y = filter_data(y)
-    f, t, Zxx = sp.signal.stft(y, 512, nperseg=512 * 10, noverlap=512 * 9)
-    plt.figure(1)
-    plt.pcolormesh(t, f, np.abs(Zxx), vmin=-1, vmax=10, shading='auto')
-    plt.title('STFT Magnitude')
-    plt.ylim(0.5, 50)
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time [sec]')
-
+def FeatureExtract(data, plot):
+    f, t, Zxx = sp.signal.stft(data, 512, nperseg=512 * 15, noverlap=512 * 14)
     delta = np.array([], dtype=float)
     theta = np.array([], dtype=float)
     alpha = np.array([], dtype=float)
@@ -62,7 +29,7 @@ def FeatureExtract(y):
     tar = np.array([], dtype=float)
     dar = np.array([], dtype=float)
     dtabr = np.array([], dtype=float)
-    for i in range(0, int(t[-1]) // 1):
+    for i in range(0, int(t[-1])):
         indices = np.where((f >= 0.5) & (f <= 4))[0]
         delta = np.append(delta, np.sum(np.abs(Zxx[indices, i])))
 
@@ -93,17 +60,42 @@ def FeatureExtract(y):
                "dar": dar,
                "dtabr": dtabr
                }
+    if plot == 1:
+        # Tạo hình ảnh chính và các hình ảnh con
+        fig = plt.figure(figsize=(12, 6))
+
+        # Hình ảnh 1
+        ax1 = fig.add_subplot(2, 2, 1)
+        ax1.plot(data)
+        ax1.set_title('EEG Raw Values')
+        ax1.set_xlabel('Samples')
+        ax1.set_ylabel('RawValue')
+
+        # Hình ảnh 2
+        ax2 = fig.add_subplot(1, 2, 2)
+        ax2.pcolormesh(t, f, np.abs(Zxx), vmin=-1, vmax=10, shading='auto')
+        ax2.set_title('STFT Magnitude')
+        ax2.set_xlabel('Time [sec]')
+        ax2.set_ylabel('Frequency [Hz]')
+        ax2.set_ylim(0.5, 40)
+
+        # Hình ảnh 3
+        ax3 = fig.add_subplot(2, 2, 3)
+        ax3.plot(diction['delta'], label="delta")
+        ax3.plot(diction['theta'], label="theta")
+        ax3.plot(diction['alpha'], label="alpha")
+        ax3.plot(diction['beta'], label="beta")
+        ax3.set_title('Frequency Bands')
+        ax3.set_xlabel('Power')
+        ax3.set_ylabel('Time [sec]')
+        ax3.legend()
+
+        # Hiển thị hình ảnh
+        plt.tight_layout()
+        plt.show()
     return diction
 
 
-feature = FeatureExtract(y)
-plt.figure(2)
-plt.plot(feature['delta'], label="delta")
-plt.plot(feature['theta'], label="theta")
-plt.plot(feature['alpha'], label="alpha")
-plt.plot(feature['beta'], label="beta")
-plt.legend()
-# plt.show()
-# df = pd.DataFrame.from_dict(feature)
-# df.to_csv("test.csv")
-# print(df)
+y = np.loadtxt("Data/Subject_1_testmonham.txt")
+y = filter_data(y)
+FeatureExtract(y, plot=1)
